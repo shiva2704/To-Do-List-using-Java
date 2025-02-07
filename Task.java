2ab4b5f3245d4814e506ce24250c20e63595430b
Shiva <134785033+shiva2704@users.noreply.github.com>
2025-02-07 13:33:25 +0530

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,28 @@
+import java.io.Serializable;
+
+public class Task implements Serializable {
+    private String description;
+    private boolean isCompleted;
+
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean completed) {
+        isCompleted = completed;
+    }
+
+    @Override
+    public String toString() {
+        return (isCompleted ? "[✔] " : "[ ] ") + description;
+    }
+}
